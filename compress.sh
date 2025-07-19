0b72b48cfc67912019e7edcc152a8df7ff62a387
Ivan Smitka <ivan@smitka.net>
2025-07-19 17:05:22 +0200

    Version Management

--- compress.sh
+++ compress.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
 sass scss/default.scss static/css/default.css
-(cd ../ && zip -r wp-meteogram/wp-meteogram.zip \
+VERSION=$(git tag -l)
+echo "${VERSION}"
+sed -i -E "s/^ \* Version\: .*$/ * Version: ${VERSION}/g" wp-meteogram.php
+(cd ../ && zip -r wp-meteogram/${VERSION}.zip \
   wp-meteogram/proxy.php \
   wp-meteogram/wp-meteogram.php \
   wp-meteogram/LICENSE \
   wp-meteogram/README.md \
-  wp-meteogram/wp-time.php \
   wp-meteogram/static)
